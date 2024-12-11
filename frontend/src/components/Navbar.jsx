c6df141a1973d4a64aa8b375487bdab5dbd29567
Suvesh Pandey <jpsuvesh29@gmail.com>
2024-12-11 19:33:29 +0530

    changed cors to all origin

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = ({notes, username, email, password, setEmail, setPassword, setUse
         <div>
             <div className='bg-[#eaf4f4] w-[100%] h-[50px] fixed flex items-center justify-between sm:px-20 px-2 py-1 top-0 z-10 '>
                 <div className='flex justify-center items-center font-bold text-lg bg-gradient-to-r from-[#1d2d44] to-[#3e5c76] bg-clip-text text-transparent  '>
-                    <img className='size-6 mr-1' src="../public/notes-icon.png" alt="" />
+                    <img className='size-6 mr-1' src="/notes-icon.png" alt="" />
                     <p>NeuraNotes</p>
                 </div>
                 
