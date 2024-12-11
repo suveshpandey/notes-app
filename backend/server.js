db1c4434729250dbc4418a3d9bb57958d6926a96
Suvesh Pandey <jpsuvesh29@gmail.com>
2024-12-11 15:53:34 +0530

    changed cors part

--- backend/server.js
+++ backend/server.js
@@ -6,28 +6,20 @@ const { userRouter } = require('./routes/user');
 
 
 const app = express();
-// Define allowed origins
-const allowedOrigins = [
-    "http://localhost:5173", // Local development
-    "https://neura-notes-frontend.onrender.com", // Production frontend
-];
 
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            // Allow requests without origin (e.g., from Postman, mobile apps)
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-    credentials: true, // Allow credentials (cookies, session, etc.)
-};
+// const corsOptions = {
+//     origin: "https://neura-notes-frontend.onrender.com",
+//     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+//     credentials: true
+// };
 
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173/",
+    methods:['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/user', userRouter);
 
