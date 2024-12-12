ce6a1de20b6bc32c2a64754a4f7281490c113e41
Suvesh Pandey <jpsuvesh29@gmail.com>
2024-12-13 02:44:34 +0530

    added a loader

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -178,7 +178,12 @@ const Home = ({username, email, password, setUsername, setEmail, setPassword}) =
     };
     
 
-    if(loading) return <div className='h-[100vh] w-[100%] pt-16 flex justify-center items center '>Fetching data, please wait...</div>;
+    // if(loading) return <div className='h-[100vh] w-[100%] pt-16 flex justify-center items center '>Fetching data, please wait...</div>;
+    if (loading) return  <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
+        <div className="w-16 h-16 border-8 border-t-8 border-gray-300 border-t-blue-500 rounded-full animate-spin">
+        </div>
+        <p className='mt-2 text-sm'>Fetching data, please wait...</p>
+    </div>
     if(error) return <div>Error: {error}</div>
 
     return (
